80c03ba7130dd6a33032768d1f63bbeef8da707f
Miroslav Bendík <miroslav.bendik@gmail.com>
2021-10-09 15:16:38 +0200

    Dropped ANDROID_TOOLS environment variable

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-#aapt package -f -I $ANDROID_PLATFORM/android.jar -J src -m -M AndroidManifest.xml -S res -v
+
 javac -d build -bootclasspath $ANDROID_PLATFORM/android.jar -classpath src -source 1.7 -target 1.7 src/sk/linuxos/*.java
 d8 build/sk/linuxos/*.class --release --output build/dex --no-desugaring
 aapt package -f -F build/CalibrateCamera.apkPart -I $ANDROID_PLATFORM/android.jar -M AndroidManifest.xml -S res -v
-CLASSPATH=$ANDROID_TOOLS/lib/* java com.android.sdklib.build.ApkBuilderMain build/CalibrateCamera.apkUnalign -d -f build/dex/classes.dex -v -z build/CalibrateCamera.apkPart
+CLASSPATH=$ANDROID_HOME/tools/lib/* java com.android.sdklib.build.ApkBuilderMain build/CalibrateCamera.apkUnalign -d -f build/dex/classes.dex -v -z build/CalibrateCamera.apkPart
 zipalign -f -v 4 build/CalibrateCamera.apkUnalign build/CalibrateCamera.apk
 
 adb install -r build/CalibrateCamera.apk
